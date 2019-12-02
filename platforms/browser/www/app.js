953c5f7e154a41ec5e9e32c9af9eb68bd407174d
dylan990222 <dylandare22@gmail.com>
2019-12-02 23:49:07 +0800

    update

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -4,16 +4,41 @@ function Add(){
     var firstNumberInteger = parseInt(firstNumber, 10);
     var secondNumberInteger = parseInt(secondNumber, 10);
 
-    if((firstNumber = "") || (secondNumber = "")){
-        console.log('if');
-        ons.notification.alert("Please enter two numbers");
+
+    // if(firstNumber || secondNumber == "") {
+    //     console.log('if');
+    //     ons.notification.alert("Please enter numbers");
+    // }
+
+    // if(firstNumber && secondNumber == ""){
+    //     console.log('both numbers');
+    //     ons.notification.alert("Please enter numbers");
+    // }
+
+
+    if(firstNumber + secondNumber == ""){
+        console.log('both numbers');
+        ons.notification.alert("Please enter numbers");
+    }
+    
+    else if(firstNumber == "") {
+        console.log('1st');
+        ons.notification.alert("Please enter first number");
     }
 
+    else if(secondNumber == "") {
+        console.log('2nd');
+        ons.notification.alert("Please enter second number");
+
+    }
+
+    
     else{
         console.log('else');
-    ons.notification.alert("Your Number is " + (firstNumberInteger + secondNumberInteger));
+        ons.notification.alert("Your Number is " + (firstNumberInteger + secondNumberInteger));
     // ons.notification.toast('Hello ' + myname, {timeout:2000}
     }
+
 }
 
 function Clear(){
